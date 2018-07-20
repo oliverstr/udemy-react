4a41a7014c66647a3df469a48db04399f72f487d
Oliver Strobel <oliver.strobel@venturus.org.br>
2018-07-20 14:55:48 -0300

    Using aux to prevent redundant div tags

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import styles from './Cockpit.css'
+import styles from './Cockpit.css';
+import Aux from '../../hoc/Aux';
 
 const Cockpit = ({people, showPeople, click, appTitle}) => {
 
     const classes = [];
-    let buttonClass = '';
+    let buttonClass = styles.Button;
 
     if(showPeople){
-        buttonClass = styles.Red;
+        buttonClass += ' ' + styles.Red;
     }
 
     if (people.length <= 2) {
@@ -18,12 +19,13 @@ const Cockpit = ({people, showPeople, click, appTitle}) => {
     }
 
     return (
-        <div className={styles.Cockpit}>
+        // Aux prevents having to create another div. It only prints whatever children it has and only works on React 16+
+        <Aux> 
             <h1>{appTitle}</h1>
             <p className={classes.join(' ')}>People names</p>
             <button className={buttonClass}
             onClick={() => click('Oliver Strobel')}>Toggle people</button>
-        </div>
+        </Aux>
     );
 }
 
